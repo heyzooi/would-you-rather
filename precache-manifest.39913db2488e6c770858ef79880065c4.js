f015d7fad4d6a0f004fe21b84121370f9d5a2f74
Victor Hugo <hey.zooi@gmail.com>
2020-02-02 23:49:14 -0800

    Updates

---
+++ precache-manifest.39913db2488e6c770858ef79880065c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5269b0f680a2f869a25362acb8aec9da",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "de7595fbb8e1a6014e26",
+    "url": "/would-you-rather/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "de7595fbb8e1a6014e26",
+    "url": "/would-you-rather/static/js/2.1a97ce62.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/would-you-rather/static/js/2.1a97ce62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e80040e5ec68d60fc8d",
+    "url": "/would-you-rather/static/js/main.cb5eb76e.chunk.js"
+  },
+  {
+    "revision": "1d8dac689adb383324ae",
+    "url": "/would-you-rather/static/js/runtime-main.dd9dcef0.js"
+  }
+]);
